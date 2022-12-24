80e0f62530560a01218b3fd0d2c100c87b83a20d
Mateus Arruda <mateusarruda232@gmail.com>
2022-12-24 12:51:39 -0300

    commit before installing threejs

--- src/components/HomeComponent/HomeComponent.js
+++ src/components/HomeComponent/HomeComponent.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 
-
+import './home-component.css'
 const HomeComponent = () => {
 
 
-    return (<div>Home Component</div>);
+    return (<div className="home-component-container">Home Component</div>);
 };
 
 export default HomeComponent;
