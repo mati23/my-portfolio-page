0dc880f29216d9de465622a489f01bcde5cf65e3
Mateus Arruda <mateusarruda232@gmail.com>
2022-12-24 17:37:36 -0300

    creating capsule

--- src/components/HomeComponent/HomeComponent.js
+++ src/components/HomeComponent/HomeComponent.js
@@ -1,10 +1,71 @@
 import React, { useEffect, useState } from "react";
+import * as THREE from "three";
+import { Fog } from "three";
+import { randFloat } from "three/src/math/MathUtils";
+import { ThreeDUtils } from "../../utils/ThreeDUtils";
+import "./home-component.css";
 
-import './home-component.css'
 const HomeComponent = () => {
+    const scene = new THREE.Scene();
+    const geometry = new THREE.BoxGeometry(1, 1, 10);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cubeList = ThreeDUtils.generateCubeList(5);
+    const renderer = new THREE.WebGLRenderer();
+    const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      );
+  
+
+  function animateCubes() {
+    requestAnimationFrame(animateCubes);
+
+    cubeList[0].rotation.x += 0.0;
+    cubeList[0].rotation.y += 0.0;
+
+    renderer.render(scene, camera);
+  }
 
 
-    return (<div className="home-component-container">Home Component</div>);
+
+  useEffect(()=>{
+    let capsule  = ThreeDUtils.generateSphere();
+    const light = new THREE.AmbientLight( 0xffffff, 2.0); // soft white light
+    light.intensity=1;
+
+    const pointLight = new THREE.PointLight(0xffff00, 2);
+    pointLight.position.x = 2
+    scene.add(pointLight);
+    
+    geometry.translate(-2,0,-10)   
+  
+    
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.getElementById("home-component-container").appendChild(renderer.domElement);
+  
+    
+    cubeList.forEach((cube)=>{
+        scene.add(cube);
+    })
+    scene.add( light );
+    scene.add(capsule);
+    scene.fog = new THREE.Fog(0x555555, 0, 12);
+
+    
+
+
+  
+    camera.position.z = 5;
+
+    animateCubes();
+  })
+  return (
+    <div className="home-component-container" id="home-component-container">
+      <div>Home Component</div>
+    </div>
+  );
 };
 
 export default HomeComponent;
---
+++ src/utils/ThreeDUtils.js
@@ -0,0 +1,65 @@
+import * as THREE from "three";
+
+const MAX = 5;
+const MIN = -5;
+
+function generateLocationsArray(numberOfCubes){
+    let locations = [];
+    for (let i=0; i<=numberOfCubes; i++ ){        
+        locations.push(i);
+    }
+    return locations;
+}
+
+function getValidLocation(listOfValidLocations){
+    let locationValue = Math.floor(Math.random()*(MAX-MIN)+(MIN));
+    let notRepeated = false;
+    while (notRepeated == false){
+        if (!listOfValidLocations.includes(locationValue)){
+            listOfValidLocations.push(locationValue)
+            notRepeated = true;
+        }else{
+            locationValue = Math.floor(Math.random()*(MAX-MIN)+(MIN));
+        }
+    }
+    return locationValue;
+}
+
+export class ThreeDUtils{   
+
+    static generateSphere(){
+        const geometry = new THREE.CapsuleGeometry( 1, 1, 4, 8 );
+        const material = new THREE.MeshStandardMaterial( {color: 0x00ff00} );
+        const capsule = new THREE.Mesh( geometry, material );
+
+        geometry.scale(0.1,0.1,0.1);
+        geometry.translate(1,1,1);
+        material.emissive=0xff0000;
+        material.emissiveIntensity=1;
+        
+        return capsule;
+    }
+    
+    static generateCubeList(numberOfCubes){
+        let locationsX = [];
+        let locationsY = [];
+        let locationsZ = [];
+
+        let cubeList = [];
+        for (let i=0; i<=numberOfCubes; i++ ){        
+            let locationX = getValidLocation(locationsX);
+            let locationY = getValidLocation(locationsY);
+            let locationZ = getValidLocation(locationsZ);
+            
+            const geometry = new THREE.BoxGeometry(1, 1, 8);            
+            const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
+
+            geometry.translate(locationX, locationY, -5);
+            let cube = new THREE.Mesh(geometry, material);            
+            cubeList.push(cube);
+        }
+        return cubeList;
+    }
+
+    
+}
