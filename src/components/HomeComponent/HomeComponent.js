10e6efb69e46103f15f499d62b117d1a1c9442c8
Mateus Arruda <mateusarruda232@gmail.com>
2022-12-26 15:24:33 -0300

    Creating smoke animation

--- src/components/HomeComponent/HomeComponent.js
+++ src/components/HomeComponent/HomeComponent.js
@@ -6,6 +6,7 @@ import { ThreeDUtils } from "../../utils/ThreeDUtils";
 import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
 import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass";
+import { AfterimagePass } from "three/examples/jsm/postprocessing/AfterimagePass";
 import "./home-component.css";
 
 const HomeComponent = () => {
@@ -13,21 +14,28 @@ const HomeComponent = () => {
   const geometry = new THREE.BoxGeometry(1, 1, 10);
   const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
   const cubeList = ThreeDUtils.generateCubeList(5);
-  const renderer = new THREE.WebGLRenderer();
+  const renderer = new THREE.WebGLRenderer({alpha:true});
   const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
   );
+  const clock = new THREE.Clock();
   const renderScene = new RenderPass(scene, camera);
   const composer = new EffectComposer(renderer);
   composer.addPass(renderScene);
   const capsule = ThreeDUtils.generateSphere();
+  const smokesArray = [];
+  let delta = clock.getDelta();
 
   const bloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 1.6, 0.8,0.8 );
   composer.addPass(bloomPass);
   composer.setSize(window.innerWidth, window.innerHeight)
+
+  const afterImagePass = new AfterimagePass();
+  afterImagePass.uniforms['damp'] = {value:0.9811};
+  composer.addPass(afterImagePass);
 
   function animateCubes() {
     requestAnimationFrame(animateCubes);
@@ -38,8 +46,36 @@ const HomeComponent = () => {
     composer.render(scene, camera);
   }
 
+  for (let p = 0; p < 150; p++) {
+    let particle = new ThreeDUtils.generateSmokeCube();
+    particle.position.set(
+      Math.random() * 400 - 250,
+      Math.random() * 400 - 250,
+      Math.random() * 1000 - 100
+    );
+    particle.rotation.z = Math.random() * 360;
+    scene.add(particle);
+    smokesArray.push(particle);
+  }
+
+  function animateSmoke() {
+    // note: three.js includes requestAnimationFrame shim
+    delta = clock.getDelta();
+    requestAnimationFrame(animateSmoke);
+    evolveSmoke();    
+  }
+  
+  function evolveSmoke() {
+    var sp = smokesArray.length;
+    while (sp--) {
+      smokesArray[sp].rotation.z += delta * 0.2;      
+    }
+  }
+
+
   useEffect(() => {
     
+
     const light = new THREE.AmbientLight(0xffffff, 0.5); // soft white light
     light.intensity = 1;
 
@@ -64,6 +100,7 @@ const HomeComponent = () => {
     camera.position.z = 5;
 
     animateCubes();
+    animateSmoke();
   });
   return (
     <div className="home-component-container" id="home-component-container">
--- src/utils/ThreeDUtils.js
+++ src/utils/ThreeDUtils.js
@@ -3,63 +3,85 @@ import * as THREE from "three";
 const MAX = 5;
 const MIN = -5;
 
-function generateLocationsArray(numberOfCubes){
-    let locations = [];
-    for (let i=0; i<=numberOfCubes; i++ ){        
-        locations.push(i);
-    }
-    return locations;
+function generateLocationsArray(numberOfCubes) {
+  let locations = [];
+  for (let i = 0; i <= numberOfCubes; i++) {
+    locations.push(i);
+  }
+  return locations;
 }
 
-function getValidLocation(listOfValidLocations){
-    let locationValue = Math.floor(Math.random()*(MAX-MIN)+(MIN));
-    let notRepeated = false;
-    while (notRepeated == false){
-        if (!listOfValidLocations.includes(locationValue)){
-            listOfValidLocations.push(locationValue)
-            notRepeated = true;
-        }else{
-            locationValue = Math.floor(Math.random()*(MAX-MIN)+(MIN));
-        }
+function getValidLocation(listOfValidLocations) {
+  let locationValue = Math.floor(Math.random() * (MAX - MIN) + MIN);
+  let notRepeated = false;
+  while (notRepeated == false) {
+    if (!listOfValidLocations.includes(locationValue)) {
+      listOfValidLocations.push(locationValue);
+      notRepeated = true;
+    } else {
+      locationValue = Math.floor(Math.random() * (MAX - MIN) + MIN);
     }
-    return locationValue;
+  }
+  return locationValue;
 }
 
-export class ThreeDUtils{   
-
-    static generateSphere(){
-        const geometry = new THREE.CapsuleGeometry( 1, 1, 4, 8 );
-        const material = new THREE.MeshPhongMaterial( {color: 0x00ff00} );
-        const capsule = new THREE.Mesh( geometry, material );
-
-        geometry.scale(0.1,0.1,0.1);
-        geometry.translate(1,1,1);
-        material.emissive=new THREE.Color(0xffffff);
-        material.emissiveIntensity=100000
-        
-        return capsule;
-    }
+export class ThreeDUtils {
+  static generateSmokeCube() {
+    const geometry = new THREE.BoxGeometry(200, 200, 200);
+    const material = new THREE.MeshLambertMaterial({
+      color: new THREE.Color("rgb(83, 84, 255)"),
+      wireframe: false,
+    });
+    const mesh = new THREE.Mesh(geometry, material);
+    const textGeo = new THREE.PlaneGeometry(300, 300);
     
-    static generateCubeList(numberOfCubes){
-        let locationsX = [];
-        let locationsY = [];
-        let locationsZ = [];
+    const textureLoader = new THREE.TextureLoader();
 
-        let cubeList = [];
-        for (let i=0; i<=numberOfCubes; i++ ){        
-            let locationX = getValidLocation(locationsX);
-            let locationY = getValidLocation(locationsY);
-            let locationZ = getValidLocation(locationsZ);
-            
-            const geometry = new THREE.BoxGeometry(1, 1, 8);            
-            const material = new THREE.MeshStandardMaterial({ color: 0x000000 });
+    const smokeTexture = textureLoader.load(
+      "https://s3-us-west-2.amazonaws.com/s.cdpn.io/95637/Smoke-Element.png"
+    );
 
-            geometry.translate(locationX, locationY, -5);
-            let cube = new THREE.Mesh(geometry, material);            
-            cubeList.push(cube);
-        }
-        return cubeList;
+    const smokeMaterial = new THREE.MeshLambertMaterial({
+        color: new THREE.Color("rgb(83, 84, 255)"),
+        map: smokeTexture,
+        transparent: true
+      });
+    const smokeGeo = new THREE.PlaneGeometry(300, 300);
+    const newMesh = new THREE.Mesh(smokeGeo, smokeMaterial);
+    return newMesh;
+  }
+
+  static generateSphere() {
+    const geometry = new THREE.CapsuleGeometry(0.2, 0.05, 4, 8);
+    const material = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+    const capsule = new THREE.Mesh(geometry, material);
+
+    geometry.scale(0.1, 0.1, 0.1);
+    geometry.translate(1, 1, 1);
+    material.emissive = new THREE.Color(0xff0000);
+    material.emissiveIntensity = 100000;
+
+    return capsule;
+  }
+
+  static generateCubeList(numberOfCubes) {
+    let locationsX = [];
+    let locationsY = [];
+    let locationsZ = [];
+
+    let cubeList = [];
+    for (let i = 0; i <= numberOfCubes; i++) {
+      let locationX = getValidLocation(locationsX);
+      let locationY = getValidLocation(locationsY);
+      let locationZ = getValidLocation(locationsZ);
+
+      const geometry = new THREE.BoxGeometry(1, 1, 8);
+      const material = new THREE.MeshStandardMaterial({ color: 0x000000 });
+
+      geometry.translate(locationX, locationY, -5);
+      let cube = new THREE.Mesh(geometry, material);
+      cubeList.push(cube);
     }
-
-    
+    return cubeList;
+  }
 }
