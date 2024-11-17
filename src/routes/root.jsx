f2ea212e59e42f8058c4b3a7d762f082b0dac745
mateus <mateusarruda232@gmail.com>
2024-11-17 19:31:49 -0300

    Add backend project

--- src/routes/root.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import NavbarComponent from '../components/NavbarComponent/NavbarComponent'
-export default function Root() {
-	return <NavbarComponent />
-}
--- src/utils/urlFormater.js
+++
@@ -1,1 +0,0 @@
-export const urlFormater = (url) => Array.isArray(url) ? url.join("/") : ""
